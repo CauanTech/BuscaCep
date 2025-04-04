7243c05b9906b10e4b62b5d2dfa7a083385f3557
Cauã Gustavo <caua_lima2023@outlook.com>
2025-04-04 20:05:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-let botao = document.getElementById('btn');
-let tarefa = document.getElementById('input-tarefa');
-let lista = document.getElementById('list');
+function coletarCep() {
+    let cep = document.getElementById("cep").value;
+    console.log(cep);
+    if(validarCEP(cep)) {
+    Dados(cep);
+    } else {
+        alert("CEP inválido. Por favor, digite um CEP válido")
+    }
+}
 
-botao.addEventListener('click', function() {
-    let paragrafo = document.createElement('p');
-    paragrafo.innerHTML = tarefa.value;
-    paragrafo.classList.add('estilo-paragrafo');
-    lista.appendChild(paragrafo);
-    paragrafo.addEventListener('click', function() {
-        paragrafo.style.textDecoration = 'line-through';
-    });
-    paragrafo.addEventListener('dblclick', function() {
-        lista.removeChild(paragrafo);
-    });
-})
+async function Dados(cep) {
+    let dados = await fetch(`https://viacep.com.br/ws/${cep}/json/`).then(response => response.json() ) 
+    dadosTela(dados);
+    
+}
+
+function dadosTela(dados) {
+document.querySelector("#endereco").innerHTML = " Endereço : " + dados.logradouro;
+    document.querySelector("#bairro").innerHTML = " Bairro : " + dados.bairro;
+    document.querySelector("#cidade").innerHTML = "Cidade : " + dados.localidade;
+    document.querySelector("#estado").innerHTML = "Estado : " + dados.uf;
+    document.querySelector("#regiao").innerHTML = "Regiao : " + dados.regiao; 
+    
+}
+
+function apagar() {
+    document.querySelector("#endereco").innerHTML = "Endereço"
+    document.querySelector("#bairro").innerHTML = "Bairro"
+    document.querySelector("#cidade").innerHTML = "Cidade"
+    document.querySelector("#estado").innerHTML = "Estado"
+    document.querySelector("#regiao").innerHTML = "Região" 
+}
+
+function validarCEP(cep) {
+    const cepPattern = /^[0-9]{5}-?[0-9]{3}$/;
+    return cepPattern.test(cep);
+}
 
